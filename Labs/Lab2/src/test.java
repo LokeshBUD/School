c16379853be23eb898eeb45a2224289bc32d7ed2
LokeshBUD <54324841+LokeshBUD@users.noreply.github.com>
2021-10-21 02:14:15 -0700

    Add files via upload

---
+++ Labs/Lab2/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[]  args){
+        int x = 7, y = 2, z = 1, w = 19;
+        x += 3;
+        y -= x;
+        z *= 4;
+        w %= 2*z;
+        System.out.prinln(w);
+
+    }
+
+}
