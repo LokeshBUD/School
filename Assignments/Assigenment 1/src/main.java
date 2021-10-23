0d14ba2174098b9bf273f8cbc9f7a80fd8e3ea00
Lokesh Budda <54324841+LokeshBUD@users.noreply.github.com>
2021-10-23 02:40:42 -0700

    Add files via upload

---
+++ Assignments/Assigenment 1/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main (String[] args){
+        String s1 = "CSE110 Java";
+        String s2;
+        System.out.println(s1.length());
+        s2 = s1.substring(7);
+        System.out.println(s2);
+
+    }
+}
